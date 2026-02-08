be06a5df402b005ad2dd8fec9821f45c1fa8618e
y29r <243280551+y29r@users.noreply.github.com>
2026-02-08 08:44:19 +0000

    Update homepage and Vite base to /portfolio

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import glsl from "vite-plugin-glsl"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), glsl()],
-	base: "/portfolio-website",
+	base: "/portfolio",
 })
